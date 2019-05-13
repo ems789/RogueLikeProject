461c72752a2dc76f8e21afeb57d5963227a5f912
ems789 <32154246+ems789@users.noreply.github.com>
2019-05-14 01:05:16 +0900

    자식 스케일을 부모 스케일과 별개로 변경하는 스크립트 추가
    부모 스케일에 따라 늘어나는 자식 스케일을 부모 스케일과 별개로 설정하도록 스크립트 작성

---
+++ Assets/My Assets/Scripts/Scaling.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform oldParent = transform.parent;
+        transform.parent = null;
+        transform.localScale = new Vector3(1,1,1);
+        transform.parent = oldParent;
+    }
+}
