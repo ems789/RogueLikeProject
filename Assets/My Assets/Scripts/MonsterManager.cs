c91dbebae47e0e60b59a87117f9e4a5271dd0bb7
ems789 <32154246+ems789@users.noreply.github.com>
2019-05-21 00:30:42 +0900

    몬스터 수를 제한하는 코드에 주석 추가

--- Assets/My Assets/Scripts/MonsterManager.cs
+++ Assets/My Assets/Scripts/MonsterManager.cs
@@ -49,7 +49,7 @@ public class MonsterManager : MonoBehaviour
                 {
                     if(Random.Range(0f, 1f) < chanceToCreateMonster)
                     {
-                        if (monsterCnt <= 5)
+                        if (monsterCnt <= 5) // 테스트용 몬스터 제한
                         {
                             monsterCnt++;
 
