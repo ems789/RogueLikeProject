26de6a1e85e2d68e5badf68b032415f2660dc3a8
ems789 <32154246+ems789@users.noreply.github.com>
2019-05-21 23:58:05 +0900

    씬 이동시 플레이어의 위치를 스타트 지점으로 이동

---
+++ Assets/My Assets/Scripts/StartPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPoint : MonoBehaviour
+{
+    void Start()
+    {
+        Transform player = FindObjectOfType<Player>().transform;
+        player.position = transform.position;
+    }
+}
