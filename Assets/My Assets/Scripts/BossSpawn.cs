d674821a87101ea3f7b15238d613b1bb8dfffaec
ems789 <32154246+ems789@users.noreply.github.com>
2019-05-22 00:13:03 +0900

    보스 스테이지 입장 시 보스 스폰 위치에 랜덤 보스 생성

---
+++ Assets/My Assets/Scripts/BossSpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawn : MonoBehaviour
+{
+    public GameObject[] bosses;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject toInstantiate = bosses[Random.Range(0, bosses.Length)];
+        GameObject instance = Instantiate(toInstantiate);
+        instance.transform.position = transform.position;
+    }
+
+}
