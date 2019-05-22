b02cf4286689f6dbd9a2f0dcfd3504a4ca1c6700
ems789 <32154246+ems789@users.noreply.github.com>
2019-05-22 23:59:36 +0900

    Chest 스크립트 추가
    플레이어가 접근하면 상자가 열림

---
+++ Assets/My Assets/Scripts/Chest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public Sprite openSprite;
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Open();
+        }
+    }
+
+    public void Open()
+    {
+        spriteRenderer.sprite = openSprite;
+    }
+}
