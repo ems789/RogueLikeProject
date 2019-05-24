3ab37e2428f2f0ea8a2928c62490fd2bd3223f14
ems789 <32154246+ems789@users.noreply.github.com>
2019-05-24 18:47:10 +0900

    (수정)상자 접근시 아이템 생성 구조 변경
    상자에서 아이템을 생성하던 것을 Item DataBase에서 생성하도록 변경

--- Assets/My Assets/Scripts/Chest.cs
+++ Assets/My Assets/Scripts/Chest.cs
@@ -6,15 +6,12 @@ public class Chest : MonoBehaviour
 {
     public Sprite openSprite;
     SpriteRenderer spriteRenderer;
-    GameObject item;
-    Item temp;
 
     private bool isOpen = false;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        item = new GameObject("temp");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,15 +28,7 @@ public class Chest : MonoBehaviour
         {
             isOpen = true;
             spriteRenderer.sprite = openSprite;
-            temp = ItemDatabase.instance.RandomItem();
-            Debug.Log(temp.itemName);
-
-            item.name = temp.itemName;
-            item.AddComponent<SpriteRenderer>().sprite = temp.image;
-            item.GetComponent<SpriteRenderer>().sortingLayerName = "Item";
-            item.AddComponent<BoxCollider2D>().isTrigger = true;
-            item.SetActive(true);
-            item.transform.position = transform.position;
+            ItemDatabase.instance.RandomItem(transform);
         }
     }
 }
--- Assets/My Assets/Scripts/ItemDatabase.cs
+++ Assets/My Assets/Scripts/ItemDatabase.cs
@@ -7,6 +7,7 @@ public class ItemDatabase : MonoBehaviour
 {
     public static ItemDatabase instance;
     public List<Item> items = new List<Item>();
+    GameObject item;
 
     private void Awake()
     {
@@ -19,8 +20,8 @@ public class ItemDatabase : MonoBehaviour
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
-        
-        //Add("Club", "몽둥이", 0, ItemType.Equipment, 1);        
+        item = new GameObject("temp");
+            
         Add("SetBWeaponBlade3", "동검", 0, ItemType.Equipment, 2);
         Add("SetCWeaponBlade4", "금검", 0, ItemType.Equipment, 3);
         Add("SetJWeaponBlade4", "철검", 0, ItemType.Equipment, 4);
@@ -32,11 +33,19 @@ public class ItemDatabase : MonoBehaviour
         items.Add(new Item(Resources.Load<SpriteAtlas>("Item/ItemAtlas").GetSprite(_spriteName), _name, _heal, _itemType ,_atk, _def, _maxHp));
     }
 
-    public Item RandomItem()
+    public void RandomItem(Transform tr)
     {
         Item temp;
 
         temp = items[Random.Range(0, items.Count)];
-        return temp;
+
+        // 게임 오브젝트에 아이템 정보를 복사
+        item.name = temp.itemName;
+        item.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+        item.AddComponent<SpriteRenderer>().sprite = temp.image;
+        item.GetComponent<SpriteRenderer>().sortingLayerName = "Item";
+        item.AddComponent<BoxCollider2D>().isTrigger = true;
+        item.SetActive(true);
+        item.transform.position = tr.position;
     }
 }
