bdfe491f64d99f4adb7673721aec9c4bf54f8d25
ems789 <32154246+ems789@users.noreply.github.com>
2019-05-26 13:05:45 +0900

    무기 습득시 캐릭터의 손 위치에 피킹

--- Assets/My Assets/Scripts/Equipment.cs
+++ Assets/My Assets/Scripts/Equipment.cs
@@ -6,12 +6,22 @@ using UnityEngine.UI;
 public class Equipment : MonoBehaviour
 {
     const int Weapon = 0, Helmet = 1, Armor = 2, Gloves =3, Boots = 4;
+    GameObject player;
     Image slot;
-    
-   public void EquipItem(Item item)
+
+    private void Start()
     {
-        if(item.image.name.Contains("Weapon"))
+        player = FindObjectOfType<Player>().gameObject;
+    }
+
+    public void EquipItem(Item item)
+    {
+        if (item.image.name.Contains("Weapon"))
+        {
             slot = transform.GetChild(Weapon).GetChild(0).GetComponent<Image>();
+            // 캐릭터가 들고 있는 무기 이미지를 교체
+            player.transform.Find("WeaponPosition(Front) (2)").GetComponent<SpriteRenderer>().sprite = item.image;                        
+        }
            
         if (item.image.name.Contains("Helmet"))
             slot = transform.GetChild(Helmet).GetChild(0).GetComponent<Image>();
