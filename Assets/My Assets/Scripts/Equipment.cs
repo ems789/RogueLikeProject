e2f600a8568030a7e89aa1f55f7d0bb88b3bfd52
ems789 <32154246+ems789@users.noreply.github.com>
2019-05-27 22:04:10 +0900

    const로 관리하던 장비 종류를 enum으로 관리하도록 변경

--- Assets/My Assets/Scripts/Equipment.cs
+++ Assets/My Assets/Scripts/Equipment.cs
@@ -5,7 +5,16 @@ using UnityEngine.UI;
 
 public class Equipment : MonoBehaviour
 {
-    const int Weapon = 0, Helmet = 1, Armor = 2, Gloves =3, Boots = 4;
+    enum EquipmentKind
+    {
+        Weapon,
+        Helmet,
+        Armor,
+        Gloves,
+        Boots,
+        Last,
+    }
+
     GameObject player;
     Image slot;
 
@@ -18,22 +27,22 @@ public class Equipment : MonoBehaviour
     {
         if (item.image.name.Contains("Weapon"))
         {
-            slot = transform.GetChild(Weapon).GetChild(0).GetComponent<Image>();
+            slot = transform.GetChild((int)EquipmentKind.Weapon).GetChild(0).GetComponent<Image>();
             // 캐릭터가 들고 있는 무기 이미지를 교체
             player.transform.Find("WeaponPosition(Front) (2)").GetComponent<SpriteRenderer>().sprite = item.image;                        
         }
            
         if (item.image.name.Contains("Helmet"))
-            slot = transform.GetChild(Helmet).GetChild(0).GetComponent<Image>();
+            slot = transform.GetChild((int)EquipmentKind.Helmet).GetChild(0).GetComponent<Image>();
 
         if (item.image.name.Contains("Armor"))
-            slot = transform.GetChild(Armor).GetChild(0).GetComponent<Image>();
+            slot = transform.GetChild((int)EquipmentKind.Armor).GetChild(0).GetComponent<Image>();
 
         if (item.image.name.Contains("Gloves"))
-            slot = transform.GetChild(Gloves).GetChild(0).GetComponent<Image>();
+            slot = transform.GetChild((int)EquipmentKind.Gloves).GetChild(0).GetComponent<Image>();
 
         if (item.image.name.Contains("Boots"))
-            slot = transform.GetChild(Boots).GetChild(0).GetComponent<Image>();
+            slot = transform.GetChild((int)EquipmentKind.Boots).GetChild(0).GetComponent<Image>();
 
         slot.sprite = item.image;
         slot.enabled = true;
