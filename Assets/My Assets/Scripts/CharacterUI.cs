5180064a6b196e7bbb74cedc4019a8bc61b9f36b
ems789 <32154246+ems789@users.noreply.github.com>
2019-06-18 03:16:39 +0900

    캐릭터 머리 위를 따라다니는 체력바 구현
    UI의 Canvas를 Wolrd Space 기준으로 표시되게 설정해서 구현,
    캐릭터 머리 위에 있는 빈 게임오브젝트를 따라다니도록 설정

---
+++ Assets/My Assets/Scripts/CharacterUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterUI : MonoBehaviour
+{
+    public Slider hpBar;
+    public GameObject headUpPosition;
+    private Player player;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        hpBar.value = (float)player.currentHP / (float)player.hp;
+        hpBar.transform.position = headUpPosition.transform.position;
+    }
+}
