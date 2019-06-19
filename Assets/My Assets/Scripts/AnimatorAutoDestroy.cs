dcd969f3d926ae0d57231a5d92803f236ab84811
ems789 <32154246+ems789@users.noreply.github.com>
2019-06-19 15:33:47 +0900

    애니메이터 자동 삭제 스크립트 추가
    애니메이터가 한번 실행 후 삭제됨

---
+++ Assets/My Assets/Scripts/AnimatorAutoDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorAutoDestroy : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
