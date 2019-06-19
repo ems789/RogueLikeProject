25b6550ee7d8f76fffb65b058272cbdd0bbeb145
ems789 <32154246+ems789@users.noreply.github.com>
2019-06-19 15:34:13 +0900

    패턴에 충돌 체크 스크립트 추가

---
+++ Assets/My Assets/Scripts/Collision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public int damage;
+
+    public float waitingTime;
+    private float chkTime = 0;
+
+    private BoxCollider2D myCollider;
+
+    void Start()
+    {
+        myCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        chkTime += Time.deltaTime;
+        while(chkTime > waitingTime)
+        {
+            myCollider.enabled = true;
+            chkTime = 0;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (collision.transform.tag == "Player")
+        {
+            Debug.Log("충돌");
+            collision.SendMessage("TakeDamage", damage);
+        }
+
+    }
+}
